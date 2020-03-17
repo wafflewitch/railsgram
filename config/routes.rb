7949e40526448c80b37852ae59bc414522c259e2
wafflewitch <jessicapruitt717@gmail.com>
2020-03-17 17:44:38 -0500

    Add routes for followers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :followers, only: [:create, :destroy], param: :user_id
+
   resources :posts do
     resource :likes, only: [:create, :destroy], module: :posts
     resources :comments, only: [:create, :new, :show, :destroy], module: :posts
   end
+
   resources :users, only: [:show], param: :username, path: '' do
     resources :posts, only: [:show], module: :users
   end
