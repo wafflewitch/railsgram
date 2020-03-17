80a36b40be8c004134799c6c5f839a4fb42588e1
wafflewitch <jessicapruitt717@gmail.com>
2020-03-17 17:29:11 -0500

    Add relationships and methods for following and unfollowing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,25 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :posts, dependent: :destroy
 
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow', dependent: :destroy
+  has_many :following, through: :following_relationships, source: :following
+
   def likes?(likeable)
     likeable.likes.where(user_id: id).any?
+  end
+
+  def follow(user_id)
+    following_relationships.find_or_create_by(following_id: user_id)
+  end
+
+  def unfollow(user_id)
+    following_relationships.where(following_id: user_id).destroy_all
+  end
+
+  def follows?(user_id)
+    following_relationships.where(following_id: user_id).any?
   end
 end
