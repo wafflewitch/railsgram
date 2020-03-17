43e02846dff36d3734ce988498eabdd9ba2d47d9
wafflewitch <jessicapruitt717@gmail.com>
2020-03-17 17:50:17 -0500

    Add controller for followers.

---
+++ app/controllers/followers_controller.rb
@@ -0,0 +1,28 @@
+class FollowerController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+
+  def create
+    if current_user.follow(@user.id)
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    if current_user.unfollow(@user.id)
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+end
